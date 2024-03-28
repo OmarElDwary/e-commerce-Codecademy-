459809c4ecd86ce8fe3c95c0bf2c1202588fa09b
Omar ElDwary <omareldwary91@gmail.com>
2024-03-28 03:11:10 +0200

    login

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const session = require('express-session');
+const passport = require('./passport-config');
 const productsRouter = require('./routes/products');
 const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login')
 
 const port = 3000
 
@@ -12,9 +15,19 @@ const upload = multer();
 app.use(bodyParser.json());
 app.use(upload.none());
 
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter)
+app.use('/login', loginRouter)
 
 
 app.listen(port)
---
+++ routes/login.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router()
+
+
+router.post('/', passport.authenticate('local', {
+    failureFlash: true
+}), (req, res) => {
+    res.status(201).json({ message: 'Logged in' });
+});
+
+module.exports = router;

c67129442a143e3f29a279142518feb8de0984b2
Omar ElDwary <omareldwary91@gmail.com>
2024-03-30 04:05:42 +0200

    cart routes

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 
-const router = express.Router()
+const router = express.Router();
 
 
 router.post('/', passport.authenticate('local', {
